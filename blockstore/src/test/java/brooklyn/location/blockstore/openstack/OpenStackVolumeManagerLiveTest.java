deac1fd55912a8a0fcf41c6a18a41e0fbc6f6cb0
Valentin Aitken <bostko@gmail.com>
2016-11-09 02:08:29 +0200

    Use same jclouds versions as in amp and the same loc parameters as if it was from location catalog

--- blockstore/src/test/java/brooklyn/location/blockstore/openstack/OpenStackVolumeManagerLiveTest.java
+++ blockstore/src/test/java/brooklyn/location/blockstore/openstack/OpenStackVolumeManagerLiveTest.java
@@ -36,18 +36,18 @@ public class OpenStackVolumeManagerLiveTest extends AbstractVolumeManagerLiveTes
         // re-using rackspace credentials, but pointing at it as a raw OpenStack nova endpoint
         Object identity = props.get(BROOKLYN_PROPERTIES_JCLOUDS_PREFIX+"openstack-cinder.identity");
         Object credential = props.get(BROOKLYN_PROPERTIES_JCLOUDS_PREFIX+"openstack-cinder.credential");
-//        Object autoGenerateKeypairs = props.get(BROOKLYN_PROPERTIES_JCLOUDS_PREFIX+"openstack-nova.auto-generate-keypairs");
+        Object autoGenerateKeypairs = props.get(BROOKLYN_PROPERTIES_JCLOUDS_PREFIX+"openstack-nova.auto-generate-keypairs");
         Object keyPair = props.get(BROOKLYN_PROPERTIES_JCLOUDS_PREFIX+"openstack-nova.keyPair");
         Object privateKeyFile = props.get(BROOKLYN_PROPERTIES_JCLOUDS_PREFIX+"openstack-nova.loginUser.privateKeyFile");
-//        Object keystoneCredentialType = props.get(BROOKLYN_PROPERTIES_JCLOUDS_PREFIX+"openstack-nova.keystone.credential-type");
+        Object keystoneCredentialType = props.get(BROOKLYN_PROPERTIES_JCLOUDS_PREFIX+"openstack-nova.jclouds.keystone.credential-type");
         props.put("brooklyn.location.named."+NAMED_LOCATION, LOCATION_SPEC);
         props.put("brooklyn.location.named."+NAMED_LOCATION+".identity", identity);
         props.put("brooklyn.location.named."+NAMED_LOCATION+".credential", credential);
         props.put("brooklyn.location.named."+NAMED_LOCATION+".region", "RegionOne");
-//        props.put("brooklyn.location.named."+NAMED_LOCATION+".jclouds.openstack-nova.auto-generate-keypairs", autoGenerateKeypairs);
+        props.put("brooklyn.location.named."+NAMED_LOCATION+".jclouds.openstack-nova.auto-generate-keypairs", autoGenerateKeypairs);
         props.put("brooklyn.location.named."+NAMED_LOCATION+".keyPair", keyPair);
         props.put("brooklyn.location.named."+NAMED_LOCATION+".loginUser.privateKeyFile", privateKeyFile);
-//        props.put("brooklyn.location.named."+NAMED_LOCATION+".credential-type", keystoneCredentialType);
+        props.put("brooklyn.location.named."+NAMED_LOCATION+".credential-type", keystoneCredentialType);
     }
 
     @Override
@@ -86,9 +86,14 @@ public class OpenStackVolumeManagerLiveTest extends AbstractVolumeManagerLiveTes
                 .put("loginUser", "centos")
                 .put("user", "amp")
                 .put("securityGroups", "VPN_local")
+                .put("auto-generate-keypairs", true)
+                .put("privateKeyFile", "~/.ssh/openstack.pem")
                 .put("templateOptions", ImmutableMap.of(
                         "networks", ImmutableList.of("426bb8f6-c8c7-4f84-ad3c-19f66b28a288")
                 ))
+                .put("cloudMachineNamer", "org.apache.brooklyn.core.location.cloud.names.CustomMachineNamer")
+                .put("minRam", "2000")
+                .put("custom.machine.namer.machine", "QA-valentin-xxxx")
                 .build());
     }
 }
