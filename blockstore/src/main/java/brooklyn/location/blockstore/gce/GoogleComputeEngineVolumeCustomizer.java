be927941f6f6f33ef5923d466301110cdeee5c12
Aled Sage <aled.sage@gmail.com>
2018-05-14 09:23:17 +0100

    Use spaces instead of tabs

--- blockstore/src/main/java/brooklyn/location/blockstore/gce/GoogleComputeEngineVolumeCustomizer.java
+++ blockstore/src/main/java/brooklyn/location/blockstore/gce/GoogleComputeEngineVolumeCustomizer.java
@@ -12,12 +12,12 @@ public class GoogleComputeEngineVolumeCustomizer {
 
     // FIXME This is Alex's guess! Untested.
     // FIXME How to set capacities, rather than just number of disks?
-	// FIXME There is no GoogleComputeEngineTemplateOptions.disks!
+    // FIXME There is no GoogleComputeEngineTemplateOptions.disks!
     public static JcloudsLocationCustomizer withNewVolume(final List<Integer> capacities) {
         return new BasicJcloudsLocationCustomizer() {
             @Override
             public void customize(JcloudsLocation location, ComputeService computeService, TemplateOptions templateOptions) {
-            	throw new UnsupportedOperationException("Unsupported for GCE");
+                throw new UnsupportedOperationException("Unsupported for GCE");
 //                Set<PersistentDisk> disks = MutableSet.of();
 //                for (int i=0; i<capacities.size(); i++) {
 //                    disks.add(new PersistentDisk(Mode.READ_WRITE, null, "sd"+(char)('f'+i), true, true));
